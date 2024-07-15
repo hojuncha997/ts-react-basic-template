4c198691b7c16a69099d605742254170604e142a
hjcha <hjcha@markany.com>
2024-07-15 14:35:21 +0900

    feat: 대시보드 라우팅 기본설정

---
+++ src/layouts/dashboard/DashboardLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+export default function DashboardLayout() {
+  return (
+    <>
+      <h2>DashboardLayout</h2>
+      <Outlet />
+    </>
+  );
+}
--- src/routes/RoutesConfig.tsx
+++ src/routes/RoutesConfig.tsx
@@ -1,5 +1,7 @@
 import { useRoutes, Navigate } from "react-router-dom";
 import MainLayout from "../layouts/main/MainLayout";
+import DashboardLayout from "../layouts/dashboard/DashboardLayout";
+
 import { LoginPage, ProductListPage } from "./elements";
 
 export default function RoutesConfig() {
@@ -11,6 +13,20 @@ export default function RoutesConfig() {
         { path: "login", element: <LoginPage /> },
         { path: "product", element: <ProductListPage /> },
         // { path: "products", element: <ProductListPage children={null} /> },
+      ],
+    },
+    {
+      path: "dashboard",
+      element: <DashboardLayout />,
+      children: [
+        { path: "login", element: <LoginPage /> },
+        {
+          path: "product",
+          children: [
+            { path: "", element: <Navigate to="list" replace /> },
+            { path: "list", element: <ProductListPage /> },
+          ],
+        },
       ],
     },
   ]);
